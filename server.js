762e81f3bb0016a8c79dd0b3682e68803f6de09a
mmoritz8 <mmoritz@stedwards.edu>
2020-02-23 00:03:20 -0600

    "yas"

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const logger = require("morgan");
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 var path = require("path");
+const routes = require("./routes");
 
 const PORT = process.env.PORT || 3000;
 
@@ -9,12 +11,12 @@ const app = express();
 
 app.use(logger("dev"));
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 
-app.use(express.static("./public"));
-
+app.use(express.static("client/build"));
+app.use(routes);
 
 require("./routes/api.js")(app);
 require("./routes/htmlRoutes.js")(app);
@@ -28,6 +30,14 @@ app.post("/submit", ({ body }, res) => {
             res.json(err);
         });
 });
+
+mongoose.Proimse = global.Promise;
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://user:001a001a@ds217548.mlab.com:17548/heroku_4lv8b7m7")
+{
+    useMongoClient: true;
+}
+);
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
